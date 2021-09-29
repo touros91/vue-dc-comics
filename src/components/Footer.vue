42a4fb97e163828e131388ea49757b0ab70dc874
touros91 <andreageraci91@gmail.com>
2021-09-29 18:07:52 +0200

    Aggiunto Footer statico

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,90 @@
 <template>
-  <footer>
-      Il mio Footer
-  </footer>
+  <div class="footer-container-top">
+    <div class="container">
+      <div class="footer-top">
+        <div class="lists-container">
+          <div class="footer-list">
+            <h4>DC COMICS</h4>
+            <ul> 
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <h4>SHOP</h4>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div class="footer-list">
+            <h4>DC</h4>
+            <ul> 
+              <li><a href="#">Terms of Use</a></li>
+              <li><a href="#">Privacy policy (New)</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="footer-list">
+            <h4>SITES</h4>
+            <ul> 
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-top-right-logo">
+          <div class="logo">
+            <img src="@/assets/images/dc-logo-bg.png" alt="DC Logo Footer">
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="footer-container-bottom">
+      <div class="container">
+        <div class="sign-up">
+          <button>SIGN-UP NOW!</button>
+        </div>
+        <div class="social">
+          <span>FOLLOW US</span>
+          <div class="facebook">
+            <img src="@/assets/images/footer-youtube.png" alt="Facebook">
+          </div>
+          <div class="twitter">
+            <img src="@/assets/images/footer-twitter.png" alt="Twitter">
+          </div>
+          <div class="youtube">
+            <img src="@/assets/images/footer-youtube.png" alt="YouTube">
+          </div>
+          <div class="pinterest">
+            <img src="@/assets/images/footer-pinterest.png" alt="Pinterest">
+          </div>
+          <div class="periscope">
+            <img src="@/assets/images/footer-periscope.png" alt="Periscope">
+          </div>
+          
+        </div>
+
+
+      </div>
+      
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,6 +94,68 @@ export default {
 </script>
 
 <style lang="scss">
+  .footer-container-top {
+    background-image: url('../assets/images/footer-bg.jpg');
+    .footer-top {
+      display: flex;
+      justify-content: space-between;
+      .lists-container {
+        display: flex;
+        .footer-list{
+          margin-right: 30px;
+          h4 {
+            color: #FEFFFF;
+            margin-bottom: 10px;
+          }
+          ul {
+            list-style: none;
+            margin-bottom: 20px;
+            a {
+              color: #767B79;  
+              text-decoration: none;
+            }
+          }
+        } 
+      } 
+      .logo {
+        max-width: 300px;
+        img{
+          width: 100%;
+        }
+      }
+    }
+  }
+  .footer-container-bottom {
+    background-color: #303030;
+    padding: 15px;
+    .container {
+      display: flex;
+      justify-content: space-between;
+      .sign-up {
+        max-width: 130px;
+        button {
+          display: inline-block;
+          padding: 10px;
+          color: #FEFFFF;
+          border: 1px solid #0282F9;
+          background-color: #303030;
 
+        }
+      }
+      .social {
+        display: flex;
+        span {
+          color: #0282F9;
+          align-self: center;
+          margin-right: 20px;
+          font-weight: bold;
+        }
+        div {
+          margin: 0 5px;
+        }
+      }
+    }
+      
+  }
 
 </style>
